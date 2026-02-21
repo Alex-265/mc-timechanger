41d9658f0446503cdc84bbfc376131edf0694367
Alex265 <alex@rundordner.com>
2026-02-21 21:58:57 +0100

    1.21.8

--- buildSrc/src/main/kotlin/com/blamejared/Versions.kt
+++ buildSrc/src/main/kotlin/com/blamejared/Versions.kt
@@ -3,10 +3,10 @@ package com.blamejared
 object Versions {
     const val MOD = "1.0.0"
     const val JAVA = "21"
-    const val MINECRAFT = "1.21.7"
+    const val MINECRAFT = "1.21.8"
     const val FABRIC_LOADER = "0.18.4" // https://fabricmc.net/develop/
-    const val FABRIC = "0.129.0+1.21.7"
-    const val NEO_FORGE = "21.7.20-beta" // https://neoforged.net/
+    const val FABRIC = "0.136.1+1.21.8"
+    const val NEO_FORGE = "21.8.50" // https://neoforged.net/
     const val NEO_FORGE_LOADER= "[4,)"
     const val MODMENU = "15.0.1" // https://modrinth.com/mod/modmenu/versions
 }
